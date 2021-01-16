49e40927aa650f50e3f8afcda9aa86f9407c91b5
Mahdi <riza.rs2000@gmail.com>
2021-01-16 18:03:52 +0330

    accounts path added to urls

--- siteOcation/urls.py
+++ siteOcation/urls.py
@@ -45,3 +45,7 @@ urlpatterns += [
 
 # Use static() to add url mapping to serve static files during development (only)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns += [
+    path('accounts/', include('django.contrib.auth.urls')),
+]
