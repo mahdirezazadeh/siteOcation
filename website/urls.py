f30547f82e38bbd432386c0f8c87cb6cf9173ad9
Mahdi <riza.rs2000@gmail.com>
2021-01-29 06:43:14 +0330

    list of websites url removed from urls

--- website/urls.py
+++ website/urls.py
@@ -4,8 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('websites/', views.WebsiteListView.as_view(), name='websites'),
-
     # pk (short for primary key) in our website model is website_domain_name
     # path(r'^website/(?P<pk>[-\w]+)$', views.WebisteDetailView.as_view(), name='website-detail'),
 ]
